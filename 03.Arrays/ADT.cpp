e39f097d55e1186cc802803f8e699bd4ab753294
AshishPal8 <ashish81301669@gmail.com>
2026-01-09 06:05:18 +0530

    insert an element in array

--- 03.Arrays/ADT.cpp
+++ 03.Arrays/ADT.cpp
@@ -23,7 +23,28 @@ public:
         for (int i = 0; i < length; i++)
         {
             cout << "Array elements: " << i << " = " << endl;
-            A[i] = i+1;
+            A[i] = i + 1;
+        }
+    }
+    void Append(int x)
+    {
+        if (length < size)
+        {
+            A[length] = x;
+            length++;
+        }
+    }
+    void Insert(int index, int x)
+    {
+        int i;
+        if (index >= 0 && index <= length)
+        {
+            for (i = length; i > index; i--)
+            {
+                A[i] = A[i - 1];
+            }
+            A[index] = x;
+            length++;
         }
     }
     void Display()
@@ -38,6 +59,8 @@ int main()
 {
     Array arr(10);
     arr.Create();
+    arr.Append(13);
+    arr.Insert(2, 15);
     arr.Display();
 
     return 0;
