ef2e38aef6b45c37a82b4344d591fadc2fc34d4b
AshishPal8 <ashish81301669@gmail.com>
2026-01-10 08:02:01 +0530

    binary search

--- 03.Arrays/ADT.cpp
+++ 03.Arrays/ADT.cpp
@@ -47,6 +47,70 @@ public:
             length++;
         }
     }
+    int Delete (int index) {
+        int x = 0;
+        int i;
+        if(index >= 0 && index <= length) {
+            for(i = index; i < length; i++){
+                A[i] = A[i+1];
+            }
+            length--;
+        }
+        return x;
+    }
+    void swapFn(int *x, int *y) {
+        int temp;
+        temp = *x;
+        *x = *y;
+        *y = temp;
+    }
+
+    int linearSearch(int x) {
+        for(int i = 0; i< length; i++){
+            if(x == A[i]){
+                // swapFn(&A[i], &A[i-1]); // transposition
+                // swapFn(&A[i], &A[0]); // move to front/head;
+                return i;
+            }
+        }
+        return -1;
+    }
+    int binarySearch(int key){
+        int l = 0;
+        int h = length;
+        int mid = 0;
+        
+        while(l <= h)
+        {
+            mid = (l+h)/2;
+            if(key == A[mid])
+                return mid;
+            else if(key < A[mid])
+                return h = mid-1;
+            else 
+                return l = mid+1;
+        }
+        return -1;
+    }
+
+    int RBinarySearch(int l, int h, int key){
+        int mid= 0;
+
+        if(l <= h){
+            mid = (l+h)/2;
+
+            if(key == A[mid]){
+                return mid;
+            } else if(key < A[mid]){
+                return RBinarySearch(l, mid-1, key);
+            } else {
+                return RBinarySearch(mid+1, h, key);
+            }
+        }
+
+        return -1;
+    }
+
     void Display()
     {
         for (int i = 0; i < length; i++)
@@ -61,6 +125,14 @@ int main()
     arr.Create();
     arr.Append(13);
     arr.Insert(2, 15);
+    // int deleted = arr.Delete(2);
+    // cout << deleted << endl;
+    int result = arr.linearSearch(15);
+    cout << "Linear Result: " << result << endl;
+    int binaryResult = arr.binarySearch(4);
+    cout << "Binary Result: " << binaryResult << endl;
+    int RbinaryResult = arr.RBinarySearch(0, 7, 4);
+    cout << "Recursive Binary Result: " << RbinaryResult << endl;
     arr.Display();
 
     return 0;
